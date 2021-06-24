49d7c7e23a80fb28df76411622048ecc1d41fa72
kyosuke-sat0 <kyosuke.sato@zealot.co.jp>
2021-06-25 00:54:39 +0900

    カートの完成

--- shop/shop_cartlook.php
+++ shop/shop_cartlook.php
@@ -30,8 +30,24 @@ else
 try
 {
 
-	$cart= $_SESSION['cart'];
-	$max=count($cart);
+	if(isset($_SESSION['cart'])==true)
+	{
+		$cart= $_SESSION['cart'];
+		$kazu=$_SESSION['kazu'];
+		$max=count($cart);
+	}
+	else
+	{
+		$max=0;
+	}
+
+	if($max==0)
+	{
+		print 'カートに商品が入っていません。<br />';
+		print '<br />';
+		print '<a href="shop_list.php"> 商品一覧へ戻る </a>';
+		exit();
+	}
 
 	$dsn = 'mysql:dbname=shop;host=localhost;charset=utf8';
 	$user = 'root';
@@ -67,23 +83,38 @@ catch (Exception $e)
 	print 'ただいま障害により大変ご迷惑をおかけしております。';
 	exit();
 }
-
 ?>
 
  カートの中身 <br />
  <br />
-<? php	for($i=0;$i<$max;$i++)
+
+ <form method="post" action="kazu_change.php">
+ <table border="1">
+ <tr>
+ <td> 商品 </td>
+ <td> 商品画像 </td>
+ <td> 価格 </td>
+ <td> 数量 </td>
+ <td> 小計 </td>
+ <td> 削除 </td>
+ </tr>
+<? php for($i=0;$i<$max;$i++)
 	{
 ?>
-	<?php	print $pro_name[$i]; ?>
-	<?php	print $pro_gazou[$i]; ?>
-	<?php	print $pro_price[$i]; ?> 円
-	<br />
+<tr>
+	<td><?php      print $pro_name[$i]; ?></td>
+	<td><?php      print $pro_gazou[$i]; ?></td>
+	<td><?php      print $pro_price[$i]; ?> 円 </td>
+	<td><input type="text" name="kazu<?php print $i; ?>" value="<?php print $kazu[$i]; ?>"></td>
+	<td><?php      print $pro_price[$i] * $kazu[$i]; ?> 円 </td>
+	<td><input type="checkbox" name="sakujo<?php print $i; ?>"></td>
+</tr>
 <? php
 	}
 ?>
-
-<form>
+</table>
+<input type="hidden" name="max" value="<?php print $max; ?>">
+<input type="submit" value="数量変更"><br /> 
 <input type="button" onclick="history.back()" value="戻る">
 </form>
 
